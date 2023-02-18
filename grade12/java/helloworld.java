2faaefe866548c157e195431c04133ae32d13621
Syed Iltefat <121273500+syxddd10001@users.noreply.github.com>
2023-02-17 20:34:14 -0500

    Create helloworld.java

---
+++ grade12/java/helloworld.java
@@ -0,0 +1,16 @@
+import java.lang.Math;
+
+class helloworld{
+	public static void main(string [] args){
+		
+		int a = 5;
+		int c = 10;
+		
+		for (int i = 0; i<5; i+=1){
+			System.out.println(a+c);
+			c-=2;
+			a-=1;
+		}
+		System.out.println("");
+	}
+}
